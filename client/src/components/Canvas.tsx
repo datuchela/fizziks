f87727b444c45faecf3d37c549b9c9db8343300b
datuchela <davit.narimanidze408@gmail.com>
2023-04-04 18:29:19 +0400

    added Canvas

--- client/src/components/Canvas.tsx
+++ client/src/components/Canvas.tsx
@@ -1,14 +1,26 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
-const DEFAULT_CANVAS_WIDTH = 800;
-const DEFAULT_CANVAS_HEIGHT = 600;
+import canvasClasses from "./Canvas.module.css";
+
+const DEFAULT_CANVAS_WIDTH = 1280;
+const DEFAULT_CANVAS_HEIGHT = 720;
 
 export const Canvas = () => {
 	const canvasRef = useRef<HTMLCanvasElement>(null);
 
+	useEffect(() => {
+		const canvas = canvasRef.current;
+		if (!canvas) return;
+		const ctx = canvas.getContext("2d");
+		if (!ctx) return;
+		// ctx.fillStyle = "white";
+		// ctx.fillRect(0, 0, canvas.width, canvas.height);
+	}, []);
+
 	return (
 		<canvas
 			ref={canvasRef}
+			className={canvasClasses.canvas}
 			width={DEFAULT_CANVAS_WIDTH}
 			height={DEFAULT_CANVAS_HEIGHT}
 		/>
