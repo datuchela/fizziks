88a0f55580cd622c595fd656dcb213c832a9cb3a
datuchela <davit.narimanidze408@gmail.com>
2023-05-14 22:22:05 +0400

    feat: added socket integration

--- client/src/hooks/useEngine.ts
+++ client/src/hooks/useEngine.ts
@@ -1,23 +1,32 @@
 import { useEffect, useRef } from "react";
 import { init, type Engine } from "../engine/init";
+import type { Socket } from "socket.io-client";
+import type { EngineObject } from "../engine/EngineState";
 
-export const useEngine = () => {
+export type EngineOptions = {
+	socket?: Socket;
+};
+
+export const useEngine = (options?: EngineOptions) => {
 	const canvasRef = useRef(null);
 	const engineState = useRef<Engine>();
 
 	useEffect(() => {
-		if (canvasRef.current) {
-			const engine = init(canvasRef.current);
+		if (!canvasRef.current) return;
+		const engine = init(canvasRef.current);
 
-			if (engine) {
-				engineState.current = engine;
-			}
-		}
+		if (!engine) return;
+		engineState.current = engine;
 	}, [canvasRef]);
 
 	const handleClick = (e: React.MouseEvent) => {
-		engineState.current?.onClickCanvas(e.clientX, e.clientY);
+		const object = engineState.current?.onClickCanvas(e.clientX, e.clientY);
+		options?.socket?.emit("spawn-object", object);
 	};
 
-	return { canvasRef, handleClick };
+	const spawnObject = (object: EngineObject) => {
+		engineState.current?.spawnObject(object);
+	};
+
+	return { canvasRef, handleClick, spawnObject };
 };
